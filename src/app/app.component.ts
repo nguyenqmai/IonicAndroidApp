6764950c96e4c7f672640308d53635dc8f1f491b
Nguyen Mai <nguyen4810@gmail.com>
2019-01-03 19:06:21 -0500

    first version

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,10 @@ import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
 
+import { FcmService } from './services/fcm.service';
+import { ToastController } from '@ionic/angular';
+
+
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html'
@@ -12,7 +16,9 @@ export class AppComponent {
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
-    private statusBar: StatusBar
+    private statusBar: StatusBar,
+    private fcm: FcmService,
+    private toastController: ToastController
   ) {
     this.initializeApp();
   }
@@ -21,6 +27,29 @@ export class AppComponent {
     this.platform.ready().then(() => {
       this.statusBar.styleDefault();
       this.splashScreen.hide();
+      this.notificationSetup();
     });
   }
+
+  private async presentToast(message) {
+    const toast = await this.toastController.create({
+      message,
+      duration: 3000
+    });
+    toast.present();
+  }
+
+  private notificationSetup() {
+    this.fcm.getToken();
+    this.fcm.onNotifications().subscribe(
+      (msg) => {
+        if (this.platform.is('ios')) {
+          this.presentToast(msg.aps.alert);
+        } else {
+          this.presentToast(msg.body);
+        }
+      });
+  }
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,14 +9,35 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { AngularFireMessagingModule } from '@angular/fire/messaging';
+import { Firebase } from '@ionic-native/firebase/ngx';
+
+
+  var config = {
+    apiKey: "AIzaSyCyCONHAJRLrxVw3HDFycEfX3BvfH087ic",
+    authDomain: "my-first-project0123.firebaseapp.com",
+    databaseURL: "https://my-first-project0123.firebaseio.com",
+    projectId: "my-first-project0123",
+    storageBucket: "my-first-project0123.appspot.com",
+    messagingSenderId: "663327712134"
+  };
+
+
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule,
+    AngularFireModule.initializeApp(config),
+    AngularFireMessagingModule
+  ],
   providers: [
     StatusBar,
     SplashScreen,
-    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    Firebase,
+    AngularFirestore
   ],
   bootstrap: [AppComponent]
 })
